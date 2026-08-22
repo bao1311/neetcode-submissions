88a88c69a7aa362c7a9d0094f16b417304a0b49b
Gia Bao Phi <105867837+bao1311@users.noreply.github.com>
2026-08-22 19:52:22 -0400

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0;
+        int ans = 0;
+        int alpha[256] = {0};
+        for (int r = 0; r < s.size(); ++r)
+        {
+            while (l < r && alpha[(unsigned char)s[r]] > 0)
+            {
+                alpha[(unsigned char)s[l]] -= 1;
+                l += 1;
+            }
+            alpha[(unsigned char)s[r]] += 1;
+            ans = max(ans, r-l+1);
+            
+        }
+        return ans;
+        
+
+    }
+};
