7f2695a9152244b7069a7138820b52da3b080754
Gia Bao Phi <105867837+bao1311@users.noreply.github.com>
2026-08-27 15:59:15 -0400

    Add: find-duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int i = nums[0];
+        int j = nums[nums[0]];
+        while (i != j)
+        {
+            i = nums[i];
+            j = nums[nums[j]];
+        }
+        i = 0;
+        while (i != j)
+        {
+            i = nums[i];
+            j = nums[j];
+        }
+        return i;
+    }
+};
